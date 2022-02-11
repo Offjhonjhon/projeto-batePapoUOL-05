ea5c53599afab82e05b2d157545a4ff79e31228b
João Araujo <jjvictorac@gmail.com>
2022-02-11 11:54:53 -0300

    pede o nome do usuario e verifica caso já esteja sendo utilizado

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
+let userNamePost = {
+    name: null
+};
 
 
 
 
-catchData();
+catchName();
 
+setInterval(sendPeriodicNameRequest, 5000);
 
+// Requisiçoes para o servidor relacionadas ao nome do usuario
+// Recebe o nome do usuario ao abrir a pagina
+function catchName() {
+    let userName = null;
+    if(userNamePost.name === null){
+        userName = prompt("Qual seu nome?");
+    }
+    else if (userNamePost.name !== null){
+        userName = prompt("Este nome de usuario já está sendo utilizado, por favor forneça outro.")
+    }
+    userNamePost = {
+        name: userName
+    } 
+    sendInicialNameRequest();
+}
+// Envia a requisição incial ao usuario
+function sendInicialNameRequest() {
+    const promisse = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants ", userNamePost);
+    promisse.then(treatSuccess);
+    promisse.catch(treatUserNameFailure);
+}
+// Trata o erro de usuario já existente e pede novamente o nome
+function treatUserNameFailure(error) {
+    const statusCode = error.response.status;
+	console.log(statusCode);
+    catchName();
+}
+// Envia o nome do usuario para o endereço de status
+function sendPeriodicNameRequest() { 
+    const promisse = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", userNamePost);
+    promisse.then(treatSuccess);
+    promisse.catch(treatFailure);
+}
 
-function catchData(){
-    const informations = axios.get
+// Trata caso a requisição seja bem sucedida
+function treatSuccess(response) {
+    const statusCode = response.status;
+    console.log(statusCode);
+}
+// Trata caso a requisiçao não seja bem sucedida
+function treatFailure(error){
+    const statusCode = error.response.status;
+    console.log(statusCode);
 }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Efeitos do menu lateral
 function openSideMenu() {
     const sideMenuBackground = document.querySelector(".side-menu-background");
     const sideMenu = document.querySelector(".side-menu");
@@ -44,12 +73,10 @@ function closeSideMenu(element){
     sideMenuBackground.style.width = "0";
     hiddenMenuText();
 }
-
 function showMenuText() {
     const sideMenu = document.querySelector(".side-menu");
     sideMenu.innerHTML = ` <h4>Escolha um contato para enviar mensagem:</h4> `
 }
-
 function hiddenMenuText() {
     const sideMenu = document.querySelector(".side-menu");
     sideMenu.innerHTML = "";
